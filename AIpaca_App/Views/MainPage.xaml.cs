42efc9762af55dc1a00acdf3b0e98b1ce0600acc
hihotz <hsb5513@naver.com>
2024-04-04 17:45:43 +0900

    mainpage UI 구성
    grid와 frame을 사용하여 UI 구성하였고, 세로모드와 가로모드의 사용성을 모두 증가시키기 위해 최적화를 하였습니다

--- AIpaca_App/Views/MainPage.xaml.cs
+++ AIpaca_App/Views/MainPage.xaml.cs
@@ -8,14 +8,50 @@
             InitializeComponent();
         }
 
-        public void OnSourceTextChanged(object sender, EventArgs e)
+        public void OnSwitchLanguageClicked(object sender, EventArgs e)
         {
 
         }
-        public void OnTranslateButtonClicked(object sender, EventArgs e)
+        public void OnEvaluateButtonClicked(object sender, EventArgs e)
         {
 
         }
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); // must be called
+
+            if (width > height)
+            {
+                // Landscape layout
+                TextFieldsGrid.RowDefinitions.Clear();
+                TextFieldsGrid.ColumnDefinitions.Clear();
+
+                TextFieldsGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+                TextFieldsGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+
+                Grid.SetRow(InputEditor.Parent as Frame, 0);
+                Grid.SetColumn(InputEditor.Parent as Frame, 0);
+
+                Grid.SetRow(TranslatedEditor.Parent as Frame, 0);
+                Grid.SetColumn(TranslatedEditor.Parent as Frame, 1);
+            }
+            else
+            {
+                // Portrait layout
+                TextFieldsGrid.RowDefinitions.Clear();
+                TextFieldsGrid.ColumnDefinitions.Clear();
+
+                TextFieldsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+                TextFieldsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+                Grid.SetRow(InputEditor.Parent as Frame, 0);
+                Grid.SetColumn(InputEditor.Parent as Frame, 0);
+
+                Grid.SetRow(TranslatedEditor.Parent as Frame, 1);
+                Grid.SetColumn(TranslatedEditor.Parent as Frame, 0);
+            }
+        }
+
     }
-
+        
 }
