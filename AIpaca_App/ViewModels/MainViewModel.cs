4ea2e3addf653ba9b31708263b23e57a5c963019
hihotz <hsb5513@naver.com>
2024-05-20 19:42:37 +0900

    gpt 버전 수정 및 메인페이지 구성요소 수정
    gemini 평가 페이지 기본 열림상태로 변경

--- AIpaca_App/ViewModels/MainViewModel.cs
+++ AIpaca_App/ViewModels/MainViewModel.cs
@@ -196,7 +196,7 @@ namespace AIpaca_App.ViewModels
                 var requestData = new
                 {
                     OpenAIAPIKey = userApiKey,
-                    GPTVersion = "GPT-4",
+                    GPTVersion = "gpt-4o",
                     Original = originalText,
                     OriginalLang = originalLang,
                     Translated = translatedText,
