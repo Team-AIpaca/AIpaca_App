c608361911f8a4a3812bbeee58a6c0af815297b5
hihotz <hsb5513@naver.com>
2024-07-04 09:27:43 +0900

    디자인 수정
    LogPage의 로그 테두리 색상 변경, 기본 색상 회색으로 변경
    API 설정 페이지 요소 크기 조정

--- AIpaca_App/Converters/SuccessToColorConverter.cs
+++ AIpaca_App/Converters/SuccessToColorConverter.cs
@@ -14,18 +14,18 @@ namespace AIpaca_App.Converters
             {
                 return successValue switch
                 {
-                    "Success" => Color.FromArgb("#76db74"),
-                    "Failed" => Color.FromArgb("#e8b079"),
+                    "Success" => Color.FromArgb("#bcffbe"),
+                    "Failed" => Color.FromArgb("#ffd3a7"),
                     _ => GetDefaultColor()     // 그 외의 경우 기본 색상 설정
                 };
             }
             return GetDefaultColor();
         }
-
+        
         private Color GetDefaultColor()
         {
             // 다크 모드에 따른 색상 반환
-            return Application.Current?.RequestedTheme == AppTheme.Dark ? Color.FromArgb("#2c2c2c") : Colors.White;
+            return Application.Current?.RequestedTheme == AppTheme.Dark ? Color.FromArgb("#2c2c2c") : Color.FromArgb("#929292");
         }
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
